735548e95f29d95a47ffdf5d5c7e8d20160f10af
Oxana Kostikova <okostikova@slcode.com>
2022-08-28 22:01:50 +0300

    Move bloc providers to wrapper

---
+++ lib/bloc_provider_wrapper.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'api_service.dart';
+import 'bloc/add_to_playlist_bloc/add_to_playlist_bloc.dart';
+import 'bloc/cacher_bloc/bloc/cacher_bloc.dart';
+import 'bloc/player_bloc/player_bloc.dart';
+import 'bloc/playlist_creation_bloc/playlist_creation_bloc.dart';
+import 'bloc/playlists_bloc/playlists_bloc.dart';
+import 'bloc/playlists_bloc/playlists_event.dart';
+import 'bloc/settings_bloc/settings_bloc.dart';
+import 'bloc/track_progress_cubit/track_progress_cubit.dart';
+import 'bloc/track_uploading_bloc/track_uploading_bloc.dart';
+import 'bloc/tracks_bloc/tracks_bloc.dart';
+import 'bloc/tracks_bloc/tracks_event.dart';
+import 'repositories/playlists_repository.dart';
+import 'repositories/tracks_repository.dart';
+
+class BlocProviderWrapper extends StatefulWidget {
+  final Widget child;
+
+  const BlocProviderWrapper({Key? key, required this.child}) : super(key: key);
+
+  @override
+  State<BlocProviderWrapper> createState() => _BlocProviderWrapperState();
+}
+
+class _BlocProviderWrapperState extends State<BlocProviderWrapper> {
+  final _apiService = ApiService();
+
+  late final _tracksRepository = TracksRepository(_apiService);
+  late final _playlistsRepository = PlaylistsRepository(_apiService);
+
+  late final _cacherBloc = CacherBloc(_apiService);
+  late final _settingsBloc = SettingsBloc(_apiService);
+  late final _tracksBloc = TracksBloc(_tracksRepository);
+  late final _playlistsBloc = PlaylistsBloc(_playlistsRepository);
+  late final _playerBloc = PlayerBloc(_apiService, _settingsBloc, _tracksRepository, _cacherBloc);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tracksBloc.add(TracksLoadEvent());
+    _playlistsBloc.add(PlaylistsLoadEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<PlayerBloc>(
+          create: (context) => _playerBloc,
+        ),
+        BlocProvider<TracksBloc>.value(
+          value: _tracksBloc,
+        ),
+        BlocProvider<TrackUploadingBloc>(
+          create: (context) => TrackUploadingBloc(_tracksRepository),
+        ),
+        BlocProvider<PlaylistsBloc>.value(
+          value: _playlistsBloc,
+        ),
+        BlocProvider<PlaylistCreationBloc>(
+          create: (context) => PlaylistCreationBloc(
+            _playlistsRepository,
+            _playlistsBloc,
+          ),
+        ),
+        BlocProvider<SettingsBloc>(
+          create: (context) => _settingsBloc,
+        ),
+        BlocProvider<AddToPlaylistBloc>(
+          create: (context) => AddToPlaylistBloc(
+            _tracksRepository,
+            _playlistsRepository,
+          ),
+        ),
+        BlocProvider<CacherBloc>.value(
+          value: _cacherBloc,
+        ),
+        BlocProvider<TrackProgressCubit>(
+          create: (context) => TrackProgressCubit(_playerBloc),
+        ),
+      ],
+      child: widget.child,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
-import 'package:basement_music/api_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
-import 'bloc/add_to_playlist_bloc/add_to_playlist_bloc.dart';
-import 'bloc/cacher_bloc/bloc/cacher_bloc.dart';
-import 'bloc/player_bloc/player_bloc.dart';
-import 'bloc/playlist_creation_bloc/playlist_creation_bloc.dart';
-import 'bloc/playlists_bloc/playlists_bloc.dart';
-import 'bloc/playlists_bloc/playlists_event.dart';
 import 'bloc/settings_bloc/settings_bloc.dart';
-import 'bloc/track_progress_cubit/track_progress_cubit.dart';
-import 'bloc/track_uploading_bloc/track_uploading_bloc.dart';
-import 'bloc/tracks_bloc/tracks_bloc.dart';
-import 'bloc/tracks_bloc/tracks_event.dart';
+import 'bloc_provider_wrapper.dart';
 import 'models/playlist.dart';
 import 'pages/home_page.dart';
 import 'pages/playlist_page.dart';
@@ -24,8 +14,6 @@ import 'pages/upload/upload_page.dart';
 import 'platform_settings/platform_settings_stub.dart'
     if (dart.library.html) 'platform_settings/platform_settings_web.dart'
     if (dart.library.io) 'platform_settings/platform_settings_io.dart';
-import 'repositories/playlists_repository.dart';
-import 'repositories/tracks_repository.dart';
 import 'routes.dart';
 import 'theme/custom_theme.dart';
 
@@ -48,71 +36,23 @@ class BasementMusic extends StatefulWidget {
 }
 
 class _BasementMusicState extends State<BasementMusic> {
-  final _apiService = ApiService();
-  late final _tracksRepository = TracksRepository(_apiService);
-  late final _playlistsRepository = PlaylistsRepository(_apiService);
-
-  late final _cacherBloc = CacherBloc(_apiService);
-  late final _settingsBloc = SettingsBloc(_apiService);
-  late final _tracksBloc = TracksBloc(_tracksRepository);
-  late final _playlistsBloc = PlaylistsBloc(_playlistsRepository);
-  late final _playerBloc = PlayerBloc(_apiService, _settingsBloc, _tracksRepository, _cacherBloc);
-
   @override
   void initState() {
     super.initState();
 
     if (kIsWeb) preventDefault();
-
-    _tracksBloc.add(TracksLoadEvent());
-    _playlistsBloc.add(PlaylistsLoadEvent());
   }
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<PlayerBloc>(
-          create: (context) => _playerBloc,
-        ),
-        BlocProvider<TracksBloc>.value(
-          value: _tracksBloc,
-        ),
-        BlocProvider<TrackUploadingBloc>(
-          create: (context) => TrackUploadingBloc(_tracksRepository),
-        ),
-        BlocProvider<PlaylistsBloc>.value(
-          value: _playlistsBloc,
-        ),
-        BlocProvider<PlaylistCreationBloc>(
-          create: (context) => PlaylistCreationBloc(
-            _playlistsRepository,
-            _playlistsBloc,
-          ),
-        ),
-        BlocProvider<SettingsBloc>(
-          create: (context) => _settingsBloc,
-        ),
-        BlocProvider<AddToPlaylistBloc>(
-          create: (context) => AddToPlaylistBloc(
-            _tracksRepository,
-            _playlistsRepository,
-          ),
-        ),
-        BlocProvider<CacherBloc>.value(
-          value: _cacherBloc,
-        ),
-        BlocProvider<TrackProgressCubit>(
-          create: (context) => TrackProgressCubit(_playerBloc),
-        ),
-      ],
+    return BlocProviderWrapper(
       child: BlocBuilder<SettingsBloc, SettingsState>(
-        builder: (context, state) {
+        builder: (context, settingsState) {
           return MaterialApp(
             title: 'Basement music',
             theme: CustomTheme.lightTheme,
             darkTheme: CustomTheme.darkTheme,
-            themeMode: state.darkTheme ? ThemeMode.dark : ThemeMode.light,
+            themeMode: settingsState.darkTheme ? ThemeMode.dark : ThemeMode.light,
             initialRoute: NavigationRoute.initial.name,
             routes: {
               NavigationRoute.settings.name: (context) => const SettingsPage(),
