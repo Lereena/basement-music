1478dc7bd067a91a37c216ce7436cbb9a4a190ce
Oxana Kostikova <okostikova@slcode.com>
2022-08-28 22:18:48 +0300

    Remove platform settings

--- lib/platform_settings/platform_settings_stub.dart
+++
@@ -1,3 +0,0 @@
-export 'platform_settings_stub.dart' if (dart.library.html) "dart:html" if (dart.libraty.io) "dart:io";
-
-void preventDefault() {}
--- lib/platform_settings/platform_settings_web.dart
+++
@@ -1,6 +0,0 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
-
-void preventDefault() {
-  document.onContextMenu.listen((event) => event.preventDefault());
-}
