0b24cf8c620673669abf85853b851db4bb1c6c73
Oxana Kostikova <okostikova@slcode.com>
2022-10-30 22:41:10 +0400

    Make navigation for playlist page

--- frontend/lib/pages/playlist_page.dart
+++ frontend/lib/pages/playlist_page.dart
@@ -1,10 +1,10 @@
-import 'package:basement_music/widgets/bottom_bar.dart';
-import 'package:basement_music/widgets/playlist_cache_action.dart';
+import 'package:basement_music/widgets/wrappers/content_narrower.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '../library.dart';
 import '../models/playlist.dart';
+import '../widgets/buttons/underlined_button.dart';
 import '../widgets/track_card.dart';
 
 class PlaylistPage extends StatefulWidget {
@@ -31,32 +31,36 @@ class _PlaylistPageState extends State<PlaylistPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.playlist.title),
-        actions: kIsWeb || widget.playlist.tracks.isEmpty
-            ? null
-            : [
-                PlaylistCacheAction(
-                  trackIds: widget.playlist.tracks.map((track) => track.id).toList(),
-                ),
-              ],
-      ),
-      body: widget.playlist.tracks.isEmpty
-          ? Center(
+    return Padding(
+      padding: kIsWeb ? const EdgeInsets.only(top: 60) : EdgeInsets.zero,
+      child: Column(
+        children: [
+          UnderlinedButton(
+            text: widget.playlist.title,
+            onPressed: () {},
+            underlined: false,
+          ),
+          if (kIsWeb) const SizedBox(height: 40) else const SizedBox(height: 10),
+          if (widget.playlist.tracks.isEmpty)
+            Center(
               child: Text(
                 'No tracks',
                 style: Theme.of(context).textTheme.bodyLarge,
               ),
             )
-          : ListView.separated(
-              separatorBuilder: (context, _) => const Divider(height: 1),
-              itemCount: widget.playlist.tracks.length,
-              itemBuilder: (context, index) => TrackCard(
-                track: widget.playlist.tracks[index],
+          else
+            ContentNarrower(
+              child: ListView.separated(
+                separatorBuilder: (context, _) => const Divider(height: 1),
+                itemCount: widget.playlist.tracks.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) => TrackCard(
+                  track: widget.playlist.tracks[index],
+                ),
               ),
             ),
-      bottomNavigationBar: BottomBar(),
+        ],
+      ),
     );
   }
 }
