0539466f51dd596e2832801763e27bc7cb0d4eb9
Oxana Kostikova <okostikova@slcode.com>
2022-10-30 22:21:32 +0400

    Fix renderFlex exception

--- frontend/lib/pages/settings_page.dart
+++ frontend/lib/pages/settings_page.dart
@@ -1,4 +1,3 @@
-import 'package:basement_music/widgets/settings/server_setting_line.dart';
 import 'package:basement_music/widgets/settings/theme_setting_line.dart';
 import 'package:basement_music/widgets/wrappers/content_narrower.dart';
 import 'package:flutter/material.dart';
@@ -13,43 +12,40 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            color: Theme.of(context).primaryColor,
-            height: 100,
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.all(15),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: DecoratedBox(
-                  decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Icon(
-                      Icons.person,
-                      size: 50,
-                      color: Theme.of(context).primaryColor,
-                    ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          color: Theme.of(context).primaryColor,
+          height: 100,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: DecoratedBox(
+                decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.person,
+                    size: 50,
+                    color: Theme.of(context).primaryColor,
                   ),
                 ),
               ),
             ),
           ),
-          ContentNarrower(
-            child: Column(
-              children: const [
-                SizedBox(height: 8),
-                ThemeSettingLine(),
-                ServerSettingLine(),
-              ],
-            ),
+        ),
+        ContentNarrower(
+          child: Column(
+            children: const [
+              SizedBox(height: 8),
+              ThemeSettingLine(),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
