1364eef664e9899d9ea17905b59fe5570dcb5de5
Oxana Kostikova <lereenadem@yandex.ru>
2023-10-19 14:25:55 +0200

    Remove titledField

--- frontend/lib/pages/upload/youtube/track_info_page.dart
+++ frontend/lib/pages/upload/youtube/track_info_page.dart
@@ -3,8 +3,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
-import '../../../widgets/titled_field.dart';
-
 class TrackInfoPage extends StatelessWidget {
   final String artist;
   final String title;
@@ -28,47 +26,52 @@ class TrackInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final inputFieldWidth = min(60.w, 400).toDouble();
+    final inputFieldWidth = min(70.w, 400).toDouble();
     artistFocusNode.requestFocus();
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Please check track info and change if incorrect',
-          style: Theme.of(context).textTheme.titleLarge,
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 20),
-        // TODO: change fields
-        TitledField(
-          title: 'Artist',
-          controller: artistController,
-          fieldWidth: inputFieldWidth,
-          focusNode: artistFocusNode,
-          onSubmitted: (_) => titleFocusNode.requestFocus(),
-        ),
-        const SizedBox(height: 20),
-        TitledField(
-          title: 'Title',
-          controller: titleController,
-          fieldWidth: inputFieldWidth,
-          focusNode: titleFocusNode,
-          onSubmitted: (_) => uploadFocusNode.requestFocus(),
-        ),
-        const SizedBox(height: 40),
-        FilledButton(
-          onPressed: () =>
-              onUploadPress(artistController.text, titleController.text),
-          focusNode: uploadFocusNode,
-          child: const Text('Upload'),
-        ),
-        const SizedBox(height: 20),
-        TextButton(
-          onPressed: onCancel,
-          child: const Text('Cancel'),
-        ),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Please check track info and change if incorrect',
+            style: Theme.of(context).textTheme.titleLarge,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: inputFieldWidth,
+            child: TextField(
+              decoration: const InputDecoration(label: Text('Artist')),
+              controller: artistController,
+              focusNode: artistFocusNode,
+              onSubmitted: (_) => titleFocusNode.requestFocus(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: inputFieldWidth,
+            child: TextField(
+              decoration: const InputDecoration(label: Text('Title')),
+              controller: titleController,
+              focusNode: titleFocusNode,
+              onSubmitted: (_) => uploadFocusNode.requestFocus(),
+            ),
+          ),
+          const SizedBox(height: 40),
+          FilledButton(
+            onPressed: () =>
+                onUploadPress(artistController.text, titleController.text),
+            focusNode: uploadFocusNode,
+            child: const Text('Upload'),
+          ),
+          const SizedBox(height: 20),
+          TextButton(
+            onPressed: onCancel,
+            child: const Text('Cancel'),
+          ),
+        ],
+      ),
     );
   }
 }
